17d9b0a825010529d66c86a7d1621be390375699
Marcelo <mrclgst10@gmail.com>
2025-04-22 22:05:11 -0300

    configure msw for mocks and implement sign-in mock

---
+++ src/api/mocks/index.ts
@@ -0,0 +1,12 @@
+import { setupWorker } from 'msw/browser'
+
+import { signInMock } from '@/api/mocks/sign-in-mock'
+import { env } from '@/env'
+
+export const worker = setupWorker(signInMock)
+
+export async function enableMSW() {
+  if (env.MODE !== 'test') return
+
+  await worker.start()
+}
