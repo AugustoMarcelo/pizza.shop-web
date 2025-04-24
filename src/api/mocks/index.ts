5c5ce20425649d89b0d1e1378faa94d37f6ec03a
Marcelo <mrclgst10@gmail.com>
2025-04-23 22:06:21 -0300

    create mocks for profile, manage restaurant, metrics and list orders

--- src/api/mocks/index.ts
+++ src/api/mocks/index.ts
@@ -1,9 +1,33 @@
 import { setupWorker } from 'msw/browser'
 
+import { getDailyRevenueInPeriodMock } from '@/api/mocks/get-daily-revenue-in-period-mock'
+import { getDayOrdersAmountMock } from '@/api/mocks/get-day-orders-amount-mock'
+import { getManagedRestaurantMock } from '@/api/mocks/get-managed-restaurant-mock'
+import { getMonthCanceledOrdersAmountMock } from '@/api/mocks/get-month-canceled-order-amount-mock'
+import { getMonthOrdersAmountMock } from '@/api/mocks/get-month-orders-amount-mock'
+import { getMonthRevenueMock } from '@/api/mocks/get-month-revenue-mock'
+import { getOrdersMock } from '@/api/mocks/get-orders-mock'
+import { getPopularProductsMock } from '@/api/mocks/get-popular-products-mock'
+import { getProfileMock } from '@/api/mocks/get-profile-mock'
+import { registerRestaurantMock } from '@/api/mocks/register-restaurant-mock'
 import { signInMock } from '@/api/mocks/sign-in-mock'
+import { updateProfileMock } from '@/api/mocks/update-profile-mock'
 import { env } from '@/env'
 
-export const worker = setupWorker(signInMock)
+export const worker = setupWorker(
+  signInMock,
+  registerRestaurantMock,
+  getDayOrdersAmountMock,
+  getMonthOrdersAmountMock,
+  getMonthCanceledOrdersAmountMock,
+  getMonthRevenueMock,
+  getDailyRevenueInPeriodMock,
+  getPopularProductsMock,
+  getProfileMock,
+  getManagedRestaurantMock,
+  updateProfileMock,
+  getOrdersMock,
+)
 
 export async function enableMSW() {
   if (env.MODE !== 'test') return
