e391105be34bc7aa8cb3667d084f957869d001bd
Marcelo <mrclgst10@gmail.com>
2025-04-26 21:40:31 -0300

    finish order details and order action mocks

--- src/api/mocks/index.ts
+++ src/api/mocks/index.ts
@@ -1,11 +1,16 @@
 import { setupWorker } from 'msw/browser'
 
+import { approveOrderMock } from '@/api/mocks/approve-order-mock'
+import { cancelOrderMock } from '@/api/mocks/cancel-order-mock'
+import { deliverOrderMock } from '@/api/mocks/deliver-order-mock'
+import { dispatchOrderMock } from '@/api/mocks/dispatch-order-mock'
 import { getDailyRevenueInPeriodMock } from '@/api/mocks/get-daily-revenue-in-period-mock'
 import { getDayOrdersAmountMock } from '@/api/mocks/get-day-orders-amount-mock'
 import { getManagedRestaurantMock } from '@/api/mocks/get-managed-restaurant-mock'
 import { getMonthCanceledOrdersAmountMock } from '@/api/mocks/get-month-canceled-order-amount-mock'
 import { getMonthOrdersAmountMock } from '@/api/mocks/get-month-orders-amount-mock'
 import { getMonthRevenueMock } from '@/api/mocks/get-month-revenue-mock'
+import { getOrderDetailsMock } from '@/api/mocks/get-order-details-mock'
 import { getOrdersMock } from '@/api/mocks/get-orders-mock'
 import { getPopularProductsMock } from '@/api/mocks/get-popular-products-mock'
 import { getProfileMock } from '@/api/mocks/get-profile-mock'
@@ -27,6 +32,11 @@ export const worker = setupWorker(
   getManagedRestaurantMock,
   updateProfileMock,
   getOrdersMock,
+  getOrderDetailsMock,
+  approveOrderMock,
+  cancelOrderMock,
+  deliverOrderMock,
+  dispatchOrderMock,
 )
 
 export async function enableMSW() {
